f2c9198c8382c375935c547386468417766d37ad
Karolina Groszewska <karolina.groszewska@gmail.com>
2024-12-06 18:22:48 -0600

    initial commit'

---
+++ BitBattles/BitBattles/BitBattlesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BitBattlesApp.swift
+//  BitBattles
+//
+//  Created by Kari Groszewska on 12/6/24.
+//
+
+import SwiftUI
+
+@main
+struct BitBattlesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
